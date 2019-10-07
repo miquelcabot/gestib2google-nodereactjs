43da1163870a37257ab29363c6b78cde4c0a1e17
miquelcabot <miquel.cabot@gmail.com>
2019-10-07 15:22:22 +0200

    Base del servidor backend

--- g2g-backend/app.js
+++ g2g-backend/app.js
@@ -1,7 +1,38 @@
-const express = require('express')
-const app = express()
-const port = 3000
+#!/usr/bin/env node
 
-app.get('/', (req, res) => res.send('Hello World!'))
+/** app.js  */
 
-app.listen(port, () => console.log(`Example app listening on port ${port}!`))
+const express = require('express');
+
+const app = express();
+
+// Definim el port i IP on s'ha d'executar the port to run on
+app.set('port', process.env.PORT || process.env.OPENSHIFT_NODEJS_PORT || 3001);
+app.set('ip', process.env.IP   || process.env.OPENSHIFT_NODEJS_IP || '0.0.0.0');
+
+// Feim log de cada request
+app.use(function(req, res, next) {
+    console.log(req.method, req.url);
+    next();
+});
+
+app.get('/', (req, res) => res.send('Servidor g2g-backend'));
+
+app.get('/importgestib', function(req, res) {
+    res
+      .status(200)
+      .json( {"importgestib" : true} );
+});
+
+app.get('/usersgestib', function(req, res) {
+    res
+      .status(200)
+      .json( {"usergestib" : true} );
+});
+
+// Escoltam les peticions
+var server = app.listen(app.get('port'), app.get('ip'), function() {
+    var port = server.address().port;
+    console.log('Servidor g2g-backend executant-se a http://%s:%s', app.get('ip'), port);
+    console.log('Pitja CTRL+C per aturar');
+});
