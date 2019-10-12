643b5643ce79f0ca349d5ea0bbb071a4059e5be9
miquelcabot <miquel.cabot@gmail.com>
2019-10-12 18:32:55 +0200

    Canviam class per className als Links

--- src/components/NavigationBar.js
+++ src/components/NavigationBar.js
@@ -5,38 +5,38 @@ class NavigationBar extends Component {
   render() {
       return (
             <nav class="navbar navbar-expand-lg navbar-dark bg-dark fixed-top" id="mainNav">
-                <Link class="navbar-brand" to="/">GestIB to Google</Link>
+                <Link className="navbar-brand" to="/">GestIB to Google</Link>
                 <button class="navbar-toggler navbar-toggler-right" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
                     <span class="navbar-toggler-icon"></span>
                 </button>
                 <div class="collapse navbar-collapse" id="navbarResponsive">
                     <ul class="navbar-nav navbar-sidenav" id="exampleAccordion">
                     <li class="nav-item" data-toggle="tooltip" data-placement="right" title="Inici">
-                        <Link class="nav-link" id="homelink" to="/">
+                        <Link className="nav-link" id="homelink" to="/">
                         <i class="fa fa-fw fa-home"></i>
                         <span class="nav-link-text">Inici</span>
                         </Link>
                     </li>
                     <li class="nav-item" data-toggle="tooltip" data-placement="right" title="Usuaris del domini">
-                        <Link class="nav-link" id="usuarisdominilink" to="/usuarisdomini">
+                        <Link className="nav-link" id="usuarisdominilink" to="/usuarisdomini">
                         <i class="fa fa-fw fa-users"></i>
                         <span class="nav-link-text">Usuaris del domini</span>
                         </Link>
                     </li>
                     <li class="nav-item" data-toggle="tooltip" data-placement="right" title="Exportar a un full de càlcul">
-                        <Link class="nav-link" id="fullcalcullink" to="/fullcalcul">
+                        <Link className="nav-link" id="fullcalcullink" to="/fullcalcul">
                         <i class="fa fa-fw fa-table"></i>
                         <span class="nav-link-text">Exportar a un full de càlcul</span>
                         </Link>
                     </li>
                     <li class="nav-item" data-toggle="tooltip" data-placement="right" title="Exportar a CSV">
-                        <Link class="nav-link" id="csvlink" to="/csv">
+                        <Link className="nav-link" id="csvlink" to="/csv">
                         <i class="fa fa-fw fa-file-text"></i>
                         <span class="nav-link-text">Exportar a CSV</span>
                         </Link>
                     </li>
                     <li class="nav-item" data-toggle="tooltip" data-placement="right" title="Importar XML">
-                        <Link class="nav-link" id="xmllink" to="/importarxml">
+                        <Link className="nav-link" id="xmllink" to="/importarxml">
                         <i class="fa fa-fw fa-file-o"></i>
                         <span class="nav-link-text">Importar XML</span>
                         </Link>
