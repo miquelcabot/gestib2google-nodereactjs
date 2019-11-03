7b76db738c32427f9f9caa3f79522b15f4322c0a
Miquel A. Cabot <miquel@MacBook-Air-de-Miquel.local>
2019-11-03 09:20:44 +0100

    Afegim llibreria googleapis

--- src/api/domainauth.js
+++ src/api/domainauth.js
@@ -1,7 +1,9 @@
+const fs = require('fs');
+const readline = require('readline');
+const {google} = require('googleapis');
+
 function getDomainAuthorization(callback) {
 
 }
 
-module.exports = {
-  getDomainAuthorization: getDomainAuthorization
-}
+export default getDomainAuthorization;
